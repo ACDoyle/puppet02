7a028586541f5c4e3ecc4a91c9f05102308b76b2
Mike Bean <mike.bean@gmail.com>
2018-12-28 21:45:20 +0100

    gg automatic change

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,10 @@ node default {
   notify {'This is the development environment': }
 
   
-
+  
   $message = hiera('message','unknown')
   notify {"Message is $message": }
+
+  $x = [ '1','2' ]
+  notify { "TEST ARRAY $x": }  
 }
