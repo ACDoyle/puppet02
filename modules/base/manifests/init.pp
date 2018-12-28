7b93826aa307772483b31faa8649b4fa8b7fe234
Mike Bean <mike.bean@gmail.com>
2018-12-28 15:45:12 +0100

    gg automatic change

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,6 +1,6 @@
 class base {
   file {'/etc/motd':
-    mode    => '0644',
+    mode    => '0664',
     owner   => '0',
     group   => '0',
     content => inline_template("Managed Node: <%= @hostname %>\nManaged by Puppet version <%= @puppetversion %>\n"),
