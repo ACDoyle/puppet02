24b1a9386bc7418fd8ff537fc35b1357c722717c
Mike Bean <mike.bean@gmail.com>
2018-12-28 14:47:34 +0100

    gg automatic change

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,12 @@
 node default {
   notify {'This is the development environment': }
 
-  file { '/tmp/hostname':
-    content => "Hello, world!\n",
-  }
   include hello
   
-  include example
 
   $message = hiera('message','unknown')
   notify {"Message is $message": }
+
   include role::webserver
 
 }
