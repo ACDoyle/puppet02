fef87c778dc9d968e4a85d1cff1ce3cf46aac973
Mike Bean <mike.bean@gmail.com>
2018-12-28 21:23:46 +0100

    gg automatic change

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,7 +3,8 @@ class profile::apache {
     default_mods        => false,
     default_confd_files =>false,
    }
-  notify {'$domain_ssl is ....\n': } ->
+  $domain = hiera('domain_ssl','unknown')
+  notify {"domain is ....": } ->
   apache::vhost { 'secure-ssl.example.com':
     port     => '443',
     docroot  => "/var/www/secure-ssl.example.com",
