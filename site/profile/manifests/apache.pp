524dad0fe504a3ae9faed4e4a8887e5158d1d5aa
Mike Bean <mike.bean@gmail.com>
2018-12-28 21:16:57 +0100

    gg automatic change

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,10 +3,10 @@ class profile::apache {
     default_mods        => false,
     default_confd_files =>false,
    }
-
-  apache::vhost { $domain_ssl:
+  notice {"$domain_ssl is ....\n": } ->
+  apache::vhost { 'secure-ssl.example.com':
     port     => '443',
-    docroot  => "/var/www/$domain_ssl",
+    docroot  => "/var/www/secure-ssl.example.com",
     ssl      => true,
   }
 }
