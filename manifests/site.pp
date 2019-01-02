2e5ffae804847727673c2f8d9b8b36e167fc6746
Mike Bean <mike.bean@gmail.com>
2019-01-02 13:20:01 +0100

    gg automatic change

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,7 +19,8 @@ node default {
 node web-d-002 {
   notify {'Hello node 2': }
   class { 'ntp': }
-  if $puppet_is_enabled { include puppet }
+#   if $puppet_is_enabled { include puppet }
+ include puppet
  # class { 'puppet':
  #   status => 'stopped',
  # }
