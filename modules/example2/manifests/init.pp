47b79213c5cd9ab77fe6ce369c44dbc14b645f3c
Mike Bean <mike.bean@gmail.com>
2019-01-16 10:53:50 +0100

    gg automatic change

--- modules/example2/manifests/init.pp
+++ modules/example2/manifests/init.pp
@@ -2,16 +2,16 @@ class example2 {
 
   file {'mcollective_server_cert':
     path   => '/etc/mcollective/ssl/mcollective_public.pem',
-    owner  => 0,
-    group  => 0,
-    mode   => 0640,
+    owner  => '0',
+    group  => '0',
+    mode   => '0640',
     source => 'puppet:///modules/example2/mcollective/mcollective_public.pem',
   }
   file {'mcollective_server_private':
     path   => '/etc/mcollective/ssl/mcollective_private.pem',
-    owner  => 0,
-    group  => 0,
-    mode   => 0600,
+    owner  => '0',
+    group  => '0',
+    mode   => '0600',
     source => 'puppet:///modules/example2/mcollective/mcollective_private.pem',
   }
   include example2::mcollective
