3d4ed8357c537484382e4d238640dc1b18f21f60
Mike Bean <mike.bean@gmail.com>
2019-01-18 15:03:01 +0100

    gg automatic change

--- modules/base/manifests/message.pp
+++ modules/base/manifests/message.pp
@@ -1,8 +1,17 @@
 class base::message (
   $message_default = "welcome in the jungle"
+  $mess_chaos =@(END)
+  #Managed by puppet - do not edit
+  first_line = x
+  second line =y
+  END
 ) {
     file { '/etc/motd_default':
       ensure  => 'file',
       content => hiera('message::message_default',$message_default),
+    }
+    file { '/tmp/chaos':
+      esnure  => 'file',
+      content => $mess_chaos,
     }
 } 
