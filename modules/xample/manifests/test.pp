33a0428249fad1cfaf225e7210c431e9a6bd200f
Mike Bean <mike.bean@gmail.com>
2019-01-19 12:38:18 +0100

    gg automatic change

--- modules/xample/manifests/test.pp
+++ modules/xample/manifests/test.pp
@@ -1,6 +1,6 @@
 class xample::test {
   notify {'Test from xample' :}
-  file { /tmp/xample.txt':
+  file { '/tmp/xample.txt':
     content => file('xample/xample.txt'),
     notify  => Class['xample::service'],
   }
