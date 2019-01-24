dbf58230978bda21fe1af38def74d6e2abe63287
Mike Bean <mike.bean@gmail.com>
2019-01-24 08:20:53 +0100

    gg automatic change

--- modules/test_params/manifests/init.pp
+++ modules/test_params/manifests/init.pp
@@ -1,5 +1,5 @@
 class test_params (
-  $varX = $test_params::params::var1i,
+  $varX = $test_params::params::var1,
 ) inherits test_params::params {
   notify { "Variable from test_params class is $varX": }
 
