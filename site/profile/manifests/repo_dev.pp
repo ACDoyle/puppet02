fcaa16e4b93b17a29dd010db6bfb57eebf906a05
Mike Bean <mike.bean@gmail.com>
2019-01-26 14:58:22 +0100

    gg automatic change

--- site/profile/manifests/repo_dev.pp
+++ site/profile/manifests/repo_dev.pp
@@ -1,3 +1,2 @@
 class profile::repo_dev {
-    include apache2
 }
--- site/role/manifests/repo_dev.pp
+++ site/role/manifests/repo_dev.pp
@@ -1,5 +1,5 @@
 class role::repo_dev {
     include profile::base
+    include profile::apache2
     include profile::repo_dev
-    include hello
 }
