51381ae19776354c2123bc876c25bf1cbd2d78d7
Mike Bean <mike.bean@gmail.com>
2019-03-05 14:30:34 +0100

    gg automatic change

--- modules/hello/manifests/init.pp
+++ modules/hello/manifests/init.pp
@@ -43,7 +43,7 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class hello {
-  $mail_info=hiera('mail_server',\undef')
+  $mail_info=hiera('mail_server','undef')
 
   file { '/tmp/hello_class':
     content => "From class",
